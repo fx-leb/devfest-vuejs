784eb025d927403f0997d6a1292ea5c20c858ea9
Francois-Xavier Lebastard <francois-xavier.lebastard@kosmos.fr>
2017-10-20 10:08:55 +0200

    step5

---
+++ src/components/Serie.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{name}}</h1> 
+        </div>{{summary}}</div>
+        <img v-bind="image.medium"/>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return 
+        {
+            name: 'NightClazz',
+            summary: 'Wha an amazing codelab...',
+            image: {medium:'static/logo.png'}
+        };
+    }
+}
+</script>
+    
+
+<style>
+
+</style>
