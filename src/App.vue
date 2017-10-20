7644eb7e4d1517e7a01af17910e3ef6985b37a44
Francois-Xavier Lebastard <francois-xavier.lebastard@kosmos.fr>
2017-10-20 15:31:41 +0200

    step4 ok

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style>
 @import '~bootstrap/dist/css/bootstrap.min.css';
-
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,14 +4,17 @@
       <div class="navbar-header">
         <a class="navbar-brand">
           <!-- Lien vers la page "liste des séries" -->
+          <router-link to="/">Home</router-link>
         </a>
       </div>
       <ul class="nav navbar-nav navbar-right">
         <li>
           <!-- Lien vers la page "liste des séries" -->
+          <router-link to="/">Liste des séries</router-link>
         </li>
         <li>
           <!-- Lien vers la page "favoris" -->
+          <router-link to="/favorites">Favoris</router-link>
         </li>
       </ul>
     </div>
