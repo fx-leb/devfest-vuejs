abe6cdbe2734f2727fe8e3e7b8829cfab617c3c3
Francois-Xavier Lebastard <francois-xavier.lebastard@kosmos.fr>
2017-10-20 16:04:11 +0200

    step 6 ok

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,15 +11,16 @@
 </template>
 
 <script>
+
 import Serie from '@/components/Serie.vue'
-import api from '@services/series.service.js'
+import api from '@/services/series.service.js'
 
 export default {
     components: {
         Serie,
         },
-      mounted(){
-          api.getSeries().then(series => series.map(serie => console.log(serie.show)))
+      mounted() {
+          api.getSeries().then(series => console.log(series.map(serie => serie.show)))
       }  
         
 }
